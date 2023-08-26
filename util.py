9e1595d65ed5343c8ab3fb9d5c17f92b223a7dae
aashir04m <108630620+aashir04m@users.noreply.github.com>
2023-08-27 04:03:35 +0500

    Add files via upload

--- util.py
+++ util.py
@@ -42,34 +42,63 @@ def classify(image, model, class_names):
     Returns:
         A tuple of the predicted class name and the confidence score for that prediction.
     """
+
+    classes = {4: ('nv', ' melanocytic nevi'),
+           6: ('mel', 'melanoma'),
+           2 :('bkl', 'benign keratosis-like lesions'),
+           1:('bcc' , ' basal cell carcinoma'),
+           5: ('vasc', ' pyogenic granulomas and hemorrhage'),
+           0: ('akiec', 'Actinic keratoses and intraepithelial carcinomae'),
+           3: ('df', 'dermatofibroma')}
     # convert image to (164, 164)
     image = ImageOps.fit(image, (28, 28), Image.Resampling.LANCZOS)
-
     # convert image to numpy array
     image_array = np.asarray(image)
-
     # normalize image
-    image_array = image_array[:, :, ::-1].copy()
     normalized_image_array = (image_array.astype(np.float32) / 255.0) - 1
+    image_array = image_array[:, :, ::-1].copy() 
+    image_array
 
     # set model input
     data = np.ndarray(shape=(1, 28, 28, 3), dtype=np.float32)
     data[0] = image_array
 
-    # make prediction
-    prediction = model.predict(data)
-    print("prediction", prediction)
-    index = np.argmax(prediction)
-    print("index", index)
-    class_name = 0 if prediction[0][0] > 0.95 else 1
+    result = model.predict(data)
+    print ("results",result)
+    max_prob = max(result[0])
 
-    class_name = "PNEUMONIA" if class_name == 0 else "NORMAL"
+    class_ind = list(result[0]).index(max_prob)
+    print ("class ind",class_ind)
+    class_name = classes[class_ind]
 
-    # if class_name == 0:
-    #     class_name = "PNEUMONIA"
-    # else:
-    #     class_name = "NORMAL"
-    # class_name = class_names[index]
-    confidence_score = prediction[0][index]
+    print(class_name)
+    # image = ImageOps.fit(image, (28, 28), Image.Resampling.LANCZOS)
 
-    return class_name, confidence_score
+    # # convert image to numpy array
+    # image_array = np.asarray(image)
+
+    # # normalize image
+    # image_array = image_array[:, :, ::-1].copy()
+    # normalized_image_array = (image_array.astype(np.float32) / 255.0) - 1
+
+    # # set model input
+    # data = np.ndarray(shape=(1, 28, 28, 3), dtype=np.float32)
+    # data[0] = image_array
+
+    # # make prediction
+    # prediction = model.predict(data)
+    # print("prediction", prediction)
+    # index = np.argmax(prediction)
+    # print("index", index)
+    # class_name = 0 if prediction[0][0] > 0.95 else 1
+
+    # class_name = "PNEUMONIA" if class_name == 0 else "NORMAL"
+
+    # # if class_name == 0:
+    # #     class_name = "PNEUMONIA"
+    # # else:
+    # #     class_name = "NORMAL"
+    # # class_name = class_names[index]
+    # confidence_score = prediction[0][index]
+
+    return class_name, 8
